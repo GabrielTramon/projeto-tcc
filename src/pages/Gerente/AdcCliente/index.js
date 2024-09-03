812cdd22624655c326fe7a2a96dd3b5d44260288
Gabriel Tramontin <133909024+BielTramon@users.noreply.github.com>
2024-09-03 15:01:36 -0300

    inicialização do crud dos clientes

--- src/pages/Gerente/AdcCliente/index.js
+++ src/pages/Gerente/AdcCliente/index.js
@@ -1,9 +1,88 @@
-import logo from "../../../assets/logo.png";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { useState } from "react";
 
-export function AdcCliente() {
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCEli15SSz40ZzsWWY3orYRcxx8I9qxIXw",
+  authDomain: "agroshop-tramontin-c28cc.firebaseapp.com",
+  projectId: "agroshop-tramontin-c28cc",
+  storageBucket: "agroshop-tramontin-c28cc.appspot.com",
+  messagingSenderId: "550256230785",
+  appId: "1:550256230785:web:fa4e1d693f4a7f7b0fe880",
+});
+
+export const AdcCliente = () => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [endereço, setEndereço] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [dataNascimento, setDataNascimento] = useState("");
+
+  const db = getFirestore(firebaseApp);
+  const usersCollectionRef = collection(db, "Clientes");
+
+  async function criarDado() {
+    try {
+      const user = await addDoc(usersCollectionRef, {
+        nome,
+        email,
+        endereço,
+        cpf,
+        telefone,
+        dataNascimento,
+      });
+
+      console.log("dados salvos com sucesso", user);
+      alert("Dados salvos com sucesso!");
+
+      // Recarregar a página
+      window.location.reload();
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      alert("Erro ao salvar os dados: " + e.message);
+    }
+  }
+
   return (
-    <div className="page">
-      <h1>Adc Cliente</h1>
+    <div>
+      <input
+        type="text"
+        placeholder="Nome"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="E-mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Endereço"
+        value={endereço}
+        onChange={(e) => setEndereço(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="CPF"
+        value={cpf}
+        onChange={(e) => setCpf(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Telefone"
+        value={telefone}
+        onChange={(e) => setTelefone(e.target.value)}
+      />
+      <input
+        type="date"
+        placeholder="Data de nascimento"
+        value={dataNascimento}
+        onChange={(e) => setDataNascimento(e.target.value)}
+      />
+      <button onClick={criarDado}>Criar dado</button>
     </div>
   );
-}
+};
--- src/pages/Gerente/CrudCliente/index.js
+++ src/pages/Gerente/CrudCliente/index.js
@@ -1,9 +1,63 @@
-import logo from "../../../assets/logo.png";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  deleteDoc,
+  getDocs,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-export function CrudCliente() {
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCEli15SSz40ZzsWWY3orYRcxx8I9qxIXw",
+  authDomain: "agroshop-tramontin-c28cc.firebaseapp.com",
+  projectId: "agroshop-tramontin-c28cc",
+  storageBucket: "agroshop-tramontin-c28cc.appspot.com",
+  messagingSenderId: "550256230785",
+  appId: "1:550256230785:web:fa4e1d693f4a7f7b0fe880",
+});
+
+export const CrudCliente = () => {
+  const [users, setUsers] = useState([]);
+
+  const db = getFirestore(firebaseApp);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, []);
+
+  async function deleteUser(id) {
+    try {
+      const userDoc = doc(db, "users", id);
+      await deleteDoc(userDoc);
+      alert("Usuário deletado com sucesso!");
+
+      // Recarregar a página
+      window.location.reload();
+    } catch (e) {
+      console.error("Erro ao deletar usuário: ", e);
+      alert("Erro ao deletar usuário: " + e.message);
+    }
+  }
+
   return (
-    <div className="page">
-      <h1>Vizualisar Cliente</h1>
+    <div>
+      <ul>
+        {users.map((user) => {
+          return (
+            <div key={user.id}>
+              <li>{user.name}</li>
+              <li>{user.email}</li>
+              <button onClick={() => deleteUser(user.id)}>Deletar</button>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
